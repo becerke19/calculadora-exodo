cc56a00368837f29f5c476a61fe2b11f5231e42f
becerke19 <becerke19@gmail.com>
2026-03-28 08:26:14 -0600

    Actualizando sw a v4 para forzar refresco

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const cacheName = 'v3-calculadora-exodo'; 
+const cacheName = 'v4-calculadora-exodo'; 
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
+    self.skipWaiting(); 
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => {
+        return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+    }));
 });
 
 self.addEventListener('fetch', e => {
