b22b52088dde4f0ab8bcf8a500d5bdae7cff0164
becerke19 <becerke19@gmail.com>
2026-03-28 09:01:41 -0600

    Actualizando sw a v8 para forzar refresco

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'v7-calculadora-exodo'; 
+const cacheName = 'v8-calculadora-exodo'; 
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
