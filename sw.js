0da9991f129c405549cc184f2c09294a0b79423e
becerke19 <becerke19@gmail.com>
2026-04-22 23:33:50 -0600

    Lanzamiento: Versión multi-operación con botones +/-

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'v9-calculadora-exodo'; 
+const cacheName = 'v10-calculadora-exodo'; 
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
